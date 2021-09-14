cfee0800743099c0e492decc5bbe6badb787743b
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-14 15:52:56 +0530

    [ShubhamPv] Adding EvenOdd

---
+++ BasicProblems5/EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems5
+{
+    class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            int num = 7;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
