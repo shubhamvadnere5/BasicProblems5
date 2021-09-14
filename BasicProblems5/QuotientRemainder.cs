38e1a1fd38e4a407ae13340c7719b4a146354bc1
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-14 16:58:21 +0530

    [ShubhamPv] Adding Quotientremainder

---
+++ BasicProblems5/QuotientRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems5
+{
+    class QuotientRemainder
+    {
+        static void Main(string[] args)
+        {
+            int Dividend = 20, Divisor = 5;
+            int Quotient = Dividend / Divisor;
+            int Remainder = Dividend % Divisor;
+            Console.WriteLine("Dividend:{0} Divisor:{1}", Dividend, Divisor);
+            Console.WriteLine("quotient=" + Quotient);
+            Console.WriteLine("Remainder=" + Remainder);
+            Console.ReadLine();
+        }
+    }
+}
