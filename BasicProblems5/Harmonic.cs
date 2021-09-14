9782007e6d1ae8b05e4bfa8b294ea3690bd70338
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-14 15:56:26 +0530

    [ShubhamPv] Adding Harmonic

---
+++ BasicProblems5/Harmonic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicproblems5
+{
+    class Harmonic
+    {
+        public static double sum = 0.0;
+
+        public static void HarmonicNumb()
+        {
+            Console.WriteLine("Enter a Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (double d = 1; d <= num; d++)
+            {
+                sum = sum + 1 / d;
+            }
+            Console.WriteLine("the Nth number series is:", +sum);
+        }
+        static void Main(string[] args)
+        {
+            Harmonic.HarmonicNumb();
+            Console.ReadLine();
+        }
+    }
+}
