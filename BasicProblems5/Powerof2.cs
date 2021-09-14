eeb4cd50ae3b285e4892acb43aec8cf101ac6d14
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-14 16:55:32 +0530

    [ShubhamPv] Adding Powerof2

---
+++ BasicProblems5/Powerof2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems5
+{
+    class Powerof2
+    {
+        public static void Powercalci()
+        {
+            Console.WriteLine("Display the Power of two");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int count = 1; count <= num; count++)
+            {
+                double pow = Math.Pow(2, count);
+                Console.WriteLine("Power of 2 of {0} is {1}:", +count, +pow);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Powerof2.Powercalci();
+            Console.ReadLine();
+        }
+    }
+}
